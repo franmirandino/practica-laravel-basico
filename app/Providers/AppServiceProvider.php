aa0dccdfee0127b8f8748f063cb90999fbc82476
Francisco Miranda <franmirandino@gmail.com>
2018-05-30 17:47:22 -0400

    Laravel avanzado 9 - Decoradores e Interfaces 2

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Repositories\CacheMessages;
+use App\Repositories\MessagesInterface;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +15,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        $this->app->bind(MessagesInterface::class, CacheMessages::class);
     }
 
     /**
